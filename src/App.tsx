de2b762d56e1058aedc89bc1edf4550b26a1d01f
sahosridoy <sahosmia.webdev@gmail.com>
2024-06-04 20:25:29 +0600

    nothing change

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import { Provider } from "react-redux";
-import store from "./app/store.ts";
 import Router from "./router/router.tsx";
-import { ThemeProvider } from "@emotion/react";
-import theme from "./theme/index.ts";
 import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
-      </Provider>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,30 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
-import {  ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme/index.ts";
+import { Provider } from "react-redux";
+import store from "./app/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-      <App />
-      <ToastContainer
-        position="bottom-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+        <ToastContainer
+          position="bottom-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
